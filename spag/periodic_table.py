0718bcfeaad31aa5ca5cd94a3dc002a898bd34b5
AYelland <amyelland3@gmail.com>
2024-10-31 14:52:27 -0400

    The merging message.

---
+++ spag/periodic_table.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+
+""" Element class for interactive shell """
+
+from __future__ import (division, print_function, absolute_import,
+                        unicode_literals)
+
+import os
+from sys import exit
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+
+################################################################################
+## Periodic Table & Dictionary
+# The periodic table ('pt') defined below is a list of the elemental symbols in the
+# periodic table. The list then used to generate a dictionary, 
+# `pt_dict`. These can be useful when imported into other scripts
+# for converting from atomic numbers to elements.
+# 
+# Example Import:
+# import spag.periodic_table as pt
+# from spag.periodic_table import pt
+# from spag.periodic_table import pt_dict
+
+
+pt_str = """H                                                  He
+                        Li Be                               B  C  N  O  F  Ne
+                        Na Mg                               Al Si P  S  Cl Ar
+                        K  Ca Sc Ti V  Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
+                        Rb Sr Y  Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I  Xe
+                        Cs Ba Lu Hf Ta W  Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
+                        Fr Ra Lr Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og"""
+
+lanthanoids    =   "La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb"
+actinoids      =   "Ac Th Pa U  Np Pu Am Cm Bk Cf Es Fm Md No"
+
+pt_str = pt_str.replace(" Ba ", " Ba " +lanthanoids+ " ") \
+    .replace(" Ra ", " Ra " +actinoids+ " ")
+del actinoids, lanthanoids
+
+pt_list = pt_str.split() # list of elements in order of atomic number
+
+## Periodic table dictionary, useful for converting atomic numbers to elements
+pt_dict = dict()
+for i, elem in enumerate(pt_list):
+    pt_dict[i+1] = elem.strip()
+
+
+################################################################################
+## Interactive Shell Query
+# ------------------------------------------------------------------------------
+# This script is a simple interactive shell that allows users to query the
+# periodic table database. The script uses SQLAlchemy to query the database and
+# return the element information. The database is a SQLite database that
+# contains the atomic number, symbol, name, and mass of each element.
+#
+# The script uses the 'Element' class to define the table schema and the
+# 'element_query' function to query the database. The 'interactive_shell'
+# function is used to run the interactive shell.
+#
+# The script can be run directly to start the interactive shell. The user can
+# enter the atomic number, symbol, name, or mass of an element to receive more
+# information about it. The user can type 'exit' or use ^C to exit the shell.
+#
+# The script can also be imported and used in other scripts. The 'element_query'
+# function can be used to query the database and return the element information.
+# This is specifically done in the 'utils_smh.py' script to query the database
+# and return the element information for the 'getelem' function.
+
+
+# Database Setup
+spag_dir = os.path.abspath(os.path.dirname(__file__))
+index = os.path.join(spag_dir, 'data/periodic_table/table.db')
+engine = sqlalchemy.create_engine(f'sqlite:///{index}')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+# Element Class for SQLAlchemy
+class Element(Base):
+    __tablename__ = 'element'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    symbol = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    atomic = sqlalchemy.Column(sqlalchemy.Integer)
+    mass = sqlalchemy.Column(sqlalchemy.Float)
+
+    def __repr__(self):
+        return f"<Element(symbol='{self.symbol}', atomic={self.atomic})>"
+
+# Function to determine input type
+def type_(input_):
+    try:
+        value = float(input_)
+        return int if value.is_integer() else float
+    except ValueError:
+        return str
+
+# Function to query element
+def element_query(_input):
+    value = type_(_input)
+
+    if value is int:
+        return session.query(Element).filter_by(atomic=_input).first()
+    if value is float:
+        return session.query(Element).filter_by(mass=_input).first()
+
+    _input = _input.capitalize()
+    if len(_input) <= 2:
+        return session.query(Element).filter_by(symbol=_input).first()
+    return session.query(Element).filter_by(name=_input).first()
+
+# Interactive Shell
+def interactive_shell():
+    attributes = ['id', 'atomic', 'symbol', 'name', 'mass']
+
+    usage = "Enter the atomic number, symbol, name, or mass of an element to recieve more \ninformation about it. (use ^C or type 'exit' to exit.)"
+    buff = '=' * 80
+    table = """    H                                                  He
+    Li Be                               B  C  N  O  F  Ne
+    Na Mg                               Al Si P  S  Cl Ar
+    K  Ca Sc Ti V  Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
+    Rb Sr Y  Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te I  Xe
+    Cs Ba    Hf Ta W  Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
+    Fr Ra    Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og
+             La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb      <-- lanthanoids 
+             Ac Th Pa U  Np Pu Am Cm Bk Cf Es Fm Md No      <-- actinoids"""
+    print()
+    print(buff)
+    print()
+    print(table)
+    print()
+    print(usage)
+
+    while True:
+        try:
+            print(buff)
+            query = input("> ")
+            if query.lower() == 'exit':
+                raise KeyboardInterrupt
+        except KeyboardInterrupt:
+            print("\nBye!")
+            exit(0)
+
+        element_ = element_query(query)
+        if element_ is None:
+            print(f"'{query}' is not valid!")
+            print()
+            print(usage)
+            continue
+
+        values = [f"{attribute}: {getattr(element_, attribute)}" for attribute in attributes]
+        for line in values:
+            print(line)
+
+# Run the interactive shell when script is called directly
+if __name__ == '__main__':
+    interactive_shell()
--- spag/read_data.py
+++ spag/read_data.py
@@ -5,13 +5,13 @@ from __future__ import (division, print_function, absolute_import,
                         unicode_literals)
 
 import pkg_resources
-import  sys, os, glob, time, IPython
-import matplotlib.pyplot as plt
-# import matplotlib.gridspec as gridspec
+import  sys, os, glob, time
 import numpy as np
 import pandas as pd
-
 import seaborn as sns
+
+from spag.convert import *
+
 sns.set_palette("colorblind")
 sns_palette = sns.color_palette()
 
@@ -24,53 +24,6 @@ script_dir = os.path.dirname(os.path.realpath(__file__))+"/" # use this if not i
 data_dir = script_dir+"data/"
 plots_dir = script_dir+"plots/"
 linelist_dir = script_dir+"linelists/"
-
-
-################################################################################
-## Utility Functions
-
-def element_matches_atomic_number(elem, Z):
-    """
-    elem: str
-        Element symbol.
-    Z: int
-        Atomic number.
-        
-    Returns True if the element symbol matches the atomic number, and False
-    otherwise.    
-    """
-    
-    element_dict = {
-        1: "H", 2: "He", 3: "Li", 4: "Be", 5: "B",
-        6: "C", 7: "N", 8: "O", 9: "F", 10: "Ne",
-        11: "Na", 12: "Mg", 13: "Al", 14: "Si", 15: "P",
-        16: "S", 17: "Cl", 18: "Ar", 19: "K", 20: "Ca",
-        21: "Sc", 22: "Ti", 23: "V", 24: "Cr", 25: "Mn",
-        26: "Fe", 27: "Co", 28: "Ni", 29: "Cu", 30: "Zn",
-        31: "Ga", 32: "Ge", 33: "As", 34: "Se", 35: "Br",
-        36: "Kr", 37: "Rb", 38: "Sr", 39: "Y", 40: "Zr",
-        41: "Nb", 42: "Mo", 43: "Tc", 44: "Ru", 45: "Rh",
-        46: "Pd", 47: "Ag", 48: "Cd", 49: "In", 50: "Sn",
-        51: "Sb", 52: "Te", 53: "I", 54: "Xe", 55: "Cs",
-        56: "Ba", 57: "La", 58: "Ce", 59: "Pr", 60: "Nd",
-        61: "Pm", 62: "Sm", 63: "Eu", 64: "Gd", 65: "Tb",
-        66: "Dy", 67: "Ho", 68: "Er", 69: "Tm", 70: "Yb",
-        71: "Lu", 72: "Hf", 73: "Ta", 74: "W", 75: "Re",
-        76: "Os", 77: "Ir", 78: "Pt", 79: "Au", 80: "Hg",
-        81: "Tl", 82: "Pb", 83: "Bi", 84: "Po", 85: "At",
-        86: "Rn", 87: "Fr", 88: "Ra", 89: "Ac", 90: "Th",
-        91: "Pa", 92: "U", 93: "Np", 94: "Pu", 95: "Am",
-        96: "Cm", 97: "Bk", 98: "Cf", 99: "Es", 100: "Fm",
-        101: "Md", 102: "No", 103: "Lr", 104: "Rf", 105: "Db",
-        106: "Sg", 107: "Bh", 108: "Hs", 109: "Mt", 110: "Ds",
-        111: "Rg", 112: "Cn", 113: "Nh", 114: "Fl", 115: "Mc",
-        116: "Lv", 117: "Ts", 118: "Og"
-    }
-    
-    if elem != element_dict[Z]:
-        return False
-    else:
-        return True
     
     
 ################################################################################
@@ -269,14 +222,14 @@ def solar_r_s_abundances():
     Reads the solar r- and s-process abundance fractions from the datafile.
     """
     
-    solar = pd.read_csv(data_dir+"solar_r_s_fractions.dat", delim_whitespace=True, names=['elem','Z','rproc','sproc','total','rfrac','sfrac'])
+    solar_rs_abund = pd.read_csv(data_dir+"solar_r_s_fractions.csv", skipinitialspace=True)
 
-    solar['logeps_r'] = np.log10(solar['rproc'])
-    solar['logeps_s'] = np.log10(solar['sproc'])
+    solar_rs_abund['logeps_r'] = np.log10(solar_rs_abund['rproc'])
+    solar_rs_abund['logeps_s'] = np.log10(solar_rs_abund['sproc'])
 
-    solar_Z = np.array(solar['Z'])
-    solar_logeps_r = np.array(solar['logeps_r'])
-    solar_logeps_s = np.array(solar['logeps_s'])
+    solar_Z = np.array(solar_rs_abund['Z'])
+    solar_logeps_r = np.array(solar_rs_abund['logeps_r'])
+    solar_logeps_s = np.array(solar_rs_abund['logeps_s'])
 
     ## Replace any NaN or Inf values with np.nan
     for i in range(len(solar_Z)):
